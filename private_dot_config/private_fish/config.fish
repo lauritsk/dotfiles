f38fec0a56e4143c9fd32b364b26629a073a2ad3
Karl Hans Laurits <karl@laurits.dev>
2026-01-12 19:14:10 +0200

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,6 +1,4 @@
 if status is-interactive
-    # set --global fish_greeting
-    # set --global fish_key_bindings fish_vi_key_bindings
     zoxide init fish --cmd cd | source
     starship init fish | source
     atuin init fish | source
