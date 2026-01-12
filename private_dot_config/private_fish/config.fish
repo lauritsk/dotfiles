a17eda81679d6346c9555c84f964eb5147537c2e
Karl Hans Laurits <karl@laurits.dev>
2026-01-12 18:21:24 +0200

    Add .config/fish/config.fish

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    set --global fish_greeting
+    set --global fish_key_bindings fish_vi_key_bindings
+    set --global --export EDITOR hx
+    set --global --export VISUAL zed
+    set --global --export PAGER bat
+    mise activate fish | source
+    fzf --fish | source
+    zoxide init fish --cmd cd | source
+    starship init fish | source
+end
+
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
