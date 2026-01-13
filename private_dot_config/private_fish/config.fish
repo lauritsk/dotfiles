c83e09b3ff03b5bb30df42c6d98a1899b1338ca3
Karl Hans Laurits <karl@laurits.dev>
2026-01-13 10:21:35 +0200

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     if test -x /opt/homebrew/bin/brew
         eval (/opt/homebrew/bin/brew shellenv)
+    else if test -x /home/linuxbrew/.linuxbrew/bin/brew
+        eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
     end
 
     if test -d /opt/homebrew/opt/ruby
