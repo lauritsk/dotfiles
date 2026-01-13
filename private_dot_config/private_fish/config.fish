a23c8961c03ae9755dc7fa92e09c28945af6ce83
Karl Hans Laurits <karl@laurits.dev>
2026-01-13 10:20:00 +0200

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,20 +1,40 @@
 if status is-interactive
-    eval (/opt/homebrew/bin/brew shellenv)
-    fish_add_path /opt/homebrew/opt/llvm/bin
-    set -gx LDFLAGS "-L/opt/homebrew/opt/llvm/lib"
-    set -gx CPPFLAGS "-I/opt/homebrew/opt/llvm/include"
-    fish_add_path /opt/homebrew/opt/rustup/bin
-    fish_add_path $HOME/.cargo/bin
-    fish_add_path $HOME/go/bin
-    fish_add_path /opt/homebrew/opt/ruby/bin
-    set -gx LDFLAGS -L/opt/homebrew/opt/ruby/lib
-    set -gx CPPFLAGS -I/opt/homebrew/opt/ruby/include
-    set -gx PKG_CONFIG_PATH /opt/homebrew/opt/ruby/lib/pkgconfig
-    mise activate fish | source
-    zoxide init fish --cmd cd | source
-    starship init fish | source
-    atuin init fish | source
-    fnox activate fish | source
+    if test -x /opt/homebrew/bin/brew
+        eval (/opt/homebrew/bin/brew shellenv)
+    end
+
+    if test -d /opt/homebrew/opt/ruby
+        fish_add_path /opt/homebrew/opt/ruby/bin
+        set -gxp LDFLAGS -L/opt/homebrew/opt/ruby/lib
+        set -gxp CPPFLAGS -I/opt/homebrew/opt/ruby/include
+        set -gxp PKG_CONFIG_PATH /opt/homebrew/opt/ruby/lib/pkgconfig
+    end
+
+    if test -d /opt/homebrew/opt/llvm
+        fish_add_path /opt/homebrew/opt/llvm/bin
+        set -gxp LDFLAGS -L/opt/homebrew/opt/llvm/lib
+        set -gxp CPPFLAGS -I/opt/homebrew/opt/llvm/include
+    end
+
+    if test -d /opt/homebrew/opt/rustup/bin
+        fish_add_path /opt/homebrew/opt/rustup/bin
+    end
+
+    if test -d $HOME/.cargo/bin
+        fish_add_path $HOME/.cargo/bin
+    end
+
+    if test -d $HOME/go/bin
+        fish_add_path $HOME/go/bin
+    end
+
+    type -q mise; and mise activate fish | source
+    type -q zoxide; and zoxide init fish --cmd cd | source
+    type -q starship; and starship init fish | source
+    type -q atuin; and atuin init fish | source
+    type -q fnox; and fnox activate fish | source
 end
 
-source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+if test -f ~/.orbstack/shell/init2.fish
+    source ~/.orbstack/shell/init2.fish
+end
