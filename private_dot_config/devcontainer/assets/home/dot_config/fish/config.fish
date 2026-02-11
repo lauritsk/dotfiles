ecd2375b8c5d09931b2a786523126f3975599b2f
Karl Hans Laurits <karl@laurits.dev>
2026-02-11 03:31:21 +0200

    Remove fish and some stuff

--- private_dot_config/devcontainer/assets/home/dot_config/fish/config.fish
+++
@@ -1,8 +0,0 @@
-if status is-interactive
-    set -gx fish_greeting
-    set -gx fish_key_bindings fish_vi_key_bindings
-    type -q mise; and mise activate fish --shims | source
-    type -q mise; and mise activate fish | source
-    type -q starship; and starship init fish | source
-    type -q zoxide; and zoxide init fish --cmd cd | source
-end
