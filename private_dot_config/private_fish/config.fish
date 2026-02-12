5abbac3cb7244e01c0fd86e669d4509e08178d9a
Karl Hans Laurits <karl@laurits.dev>
2026-02-12 15:14:45 +0200

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,6 +1,9 @@
 if status is-interactive
     set -gx fish_greeting
     set -gx fish_key_bindings fish_vi_key_bindings
+    eval (/opt/homebrew/bin/brew shellenv)
     type -q starship; and starship init fish | source
     type -q zoxide; and zoxide init fish --cmd cd | source
 end
+
+test -f ~/.orbstack/shell/init.fish; and source ~/.orbstack/shell/init.fish
