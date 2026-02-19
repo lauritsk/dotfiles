d9fe74af2ecee5ed1fe706869b5c55a2c0ef9a6c
Karl Hans Laurits <karl@laurits.dev>
2026-02-19 18:54:23 +0100

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -8,3 +8,7 @@ if status is-interactive
 end
 
 test -f ~/.orbstack/shell/init.fish; and source ~/.orbstack/shell/init.fish
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
