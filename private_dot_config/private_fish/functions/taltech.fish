801e8bfc8d6d73c8a2e205064bba0b9f0151c04e
Karl Hans Laurits <karl@laurits.dev>
2026-02-20 14:24:25 +0100

    Remove .config/fish/functions/taltech.fish

--- private_dot_config/private_fish/functions/taltech.fish
+++
@@ -1,3 +0,0 @@
-function taltech --wraps='sudo openfortivpn vpn.taltech.ee:443 --saml-login' --description 'alias taltech sudo openfortivpn vpn.taltech.ee:443 --saml-login'
-    sudo openfortivpn vpn.taltech.ee:443 --saml-login $argv
-end
