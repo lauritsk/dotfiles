0f90df9a0140d9ca7c06c2e3ba9985e1ee0610d5
Karl Hans Laurits <karl@laurits.dev>
2026-02-20 14:51:34 +0100

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -3,8 +3,10 @@ if status is-interactive
     set -gx fish_greeting
     set -gx fish_key_bindings fish_vi_key_bindings
     eval (/opt/homebrew/bin/brew shellenv)
+    type -q mise; and mise activate fish | source
     type -q starship; and starship init fish | source
     type -q zoxide; and zoxide init fish --cmd cd | source
+else
+    type -q mise; and mise activate fish --shims | source
+    source ~/.orbstack/shell/init2.fish 2>/dev/null || :
 end
-
-source ~/.orbstack/shell/init2.fish 2>/dev/null || :
