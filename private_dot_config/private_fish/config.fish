b83480bfb105c1e94faf55ea7844a3be37639c19
Karl Hans Laurits <karl@laurits.dev>
2026-09-12 08:49:11 +0000

    Remove .config/gh/hosts.yml Remove .config/gh/config.yml Remove .config/helix/config.toml Update .config/fish/config.fish Remove .config/fish/functions/ld.fish Remove .config/fish/functions/y.fish Update .config/mise/config.toml Update .config/mise/mise.lock Remove .config/yazi/yazi.toml

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,8 +1,6 @@
 if status is-interactive
     set -g fish_greeting
     set -g fish_key_bindings fish_vi_key_bindings
-    fish_add_path /home/linuxbrew/.linuxbrew/bin
-    fish_add_path /home/linuxbrew/.linuxbrew/sbin
     mise activate fish | source
     zoxide init fish --cmd cd | source
 end
